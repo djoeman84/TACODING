299a7104471b0341836b5873af82638b687b49fd
Joseph Delgado <jdelgado@flatiron.com>
2015-03-24 23:31:03 -0400

    adding playground starter files

---
+++ playground/BlankKarel/BlankKarel.java
@@ -0,0 +1,19 @@
+/*
+ * File: BlankKarel.java
+ * ---------------------
+ * This is a blank Karel project that you can change at will. Remember, if you change
+ * the class name, you'll need to change the filename so that it matches.
+ * If you want to make the program load with a specific world, make the
+ * world file named the same as this class. For example, when this program is
+ * run, it would try to load BlankKarel.w if there is one.
+ */
+
+import acm.program.*;
+import stanford.karel.*;
+
+public class BlankKarel extends Karel {
+	public void run() {
+		/* You fill this in */
+	}
+}
+
---
+++ playground/BlankProject/BlankClass.java
@@ -0,0 +1,16 @@
+/*
+ * File: BlankClass.java
+ * ---------------------
+ * This class is a blank one that you can change at will. Remember, if you change
+ * the class name, you'll need to change the filename so that it matches.
+ * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
+ */
+
+import acm.program.*;
+
+public class BlankClass extends ConsoleProgram {
+	public void run() {
+		/* You fill this in */
+	}
+}
+
