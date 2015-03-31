2b00d151e13d993d02e7ef8cca6fc46dd6ab8f92
Sarah Sypris <ssypris@gmail.com>
2015-03-30 23:50:16 -0400

    Attempted final implementations of Midpoint and Checkerboard
    Worked on Midpoint and Checkerboard final implementations.
    Caring helped some with indenting and commenting.
    Carino loves carina yes he does ruff

--- 1 Karel the Robot/Code/CheckerboardKarel.java
+++ 1 Karel the Robot/Code/CheckerboardKarel.java
@@ -11,6 +11,131 @@ import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
 
-	// You fill in this part
+	// Will create a checkerboard on any blank world so long as it is four sided
+	public void run() {
+		// HINT: Honey this is great work you is doing:)
+		// I don't want to write in too big of hints here
+		// or influence your thinking here too much. I'll
+		// text you from work and maybe we can work on a
+		// a simpler way to do this
+		//
+		// Great decomposition and implementation using
+		// all of the things we talked about yesterday:)
+		// You is really the smartest peekin I know I am
+		// thouroughly impressed:)
+		//
+		// My hint is this, this might be too vauge but
+		// oh well.
+		//    In this one it may be best to forget any
+		//    notion of efficiency of the program.
+		//    I would recommend trying to rewrite
+		//    firstRow as checkerRowOrColumn() as I
+		//    detailed below. and make sure it matches
+		//    the javadoc.
+		//    If you can test this in isolation like
+		//    we did together to make sure sure sure
+		//    it works, I think it can help you build
+		//    the program more easily
+		createFirstRow ();
+		createAllOtherRows();
+	}
+	
+	/**
+	 * Fills in every other corner in a given row/column
+	 * @before: Karel is facing any direction
+	 * @after: Karel is in the same position facing the
+	 * 			same direction.
+	 * 		   Beepers have been placed on every other
+	 * 			position in front of where Karel was
+	 * 			standing
+	 */
+	private void checkerRowOrColumn() {
+		// TODO: nale makes the great codes to this
+	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
+	private void createFirstRow() {
+		while (frontIsClear()) {
+			putBeeper();
+			moveTwice();
+		}
+		if (frontIsBlocked() && beepersPresent()) {
+			faceNorth();
+			move();
+			turnRight();
+		} else {
+			faceNorth();
+			move();
+			putBeeper();
+			turnLeft();		
+		}
+											
+	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
+	private void moveTwice() {
+		if (frontIsClear()) {
+			move();
+		}
+		if (frontIsClear()) {
+			move();
+		}
+	}
+	
+	/**
+	 * Ensures that Karel is facing North
+	 * @before: Karel is facing in any direction (NWSE)
+	 * @after: Karel is facing North
+	 */
+	private void faceNorth() {
+		while (notFacingNorth()) {
+			turnRight();
+		}
+	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
+	private void createAllOtherRows() {
+		if (beepersPresent()) {
+			while (frontIsClear()) {
+				moveTwice();
+				putBeeper();
+			}
+			faceNorth();
+			move();
+			aboutFace();
+		}
+		putBeeper();
+		while (frontIsClear()) {
+			moveTwice();
+			putBeeper();
+		}
+		faceNorth();
+		move();
+		aboutFace();
+	
+	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
+	private void aboutFace() {
+		if (frontIsBlocked()) {
+			turnAround();
+		}
+	}
 
 }
--- 1 Karel the Robot/Code/MidpointFindingKarel.java
+++ 1 Karel the Robot/Code/MidpointFindingKarel.java
@@ -13,11 +13,21 @@
 import stanford.karel.*;
 
 public class MidpointFindingKarel extends SuperKarel {
-
+		// this program finds the midpoint in any world that is as wide as it is tall
+	
 	public void run() {
 		findMidPoint();
 		deleteNonMidPointBeepers();
 	}
+	
+	/**
+	 * Distributes beepers across the whole board
+	 * @before: Karel is in the SW corner of the board facing E
+	 * 			and the board is empty
+	 * @after: Karel is standing on one of the center positions
+	 * 			on the S border of he board, and the bottom row
+	 * 			is covered with tiles
+	 */
 	private void findMidPoint() {
 		while (noBeepersPresent()) {
 			moveToNoBeeper();
@@ -25,9 +35,19 @@ public class MidpointFindingKarel extends SuperKarel {
 			turnAround();
 			move();
 		}
-		putBeeper();
-		
+		// HINT: we talked about a special
+		// case where Karel wouldn't meet the after state detailed
+		// above. See if you can write code that will fix it only
+		// if it is in that state here (after the while loop)
 	}
+	
+	/**
+	 * Moves Karel forward until she has found the last position
+	 * without either a Beeper or a wall.
+	 * @before: Karel is facing any direction anywhere on the board
+	 * @after: Karel is facing the same direction in the last nonBeeper/
+	 * 			non-final position in that row/column
+	 */
 	private void moveToNoBeeper() {
 		while (frontIsClear() && noBeepersPresent()) {
 			move();
@@ -36,6 +56,12 @@ public class MidpointFindingKarel extends SuperKarel {
 			backup();
 		}
 	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
 	private void backup() {
 		turnAround();
 		if (frontIsClear()) {
@@ -43,29 +69,50 @@ public class MidpointFindingKarel extends SuperKarel {
 		}
 		turnAround();
 	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
 	private void deleteNonMidPointBeepers() {
 		faceEast();
-		putExtraBeeper();
+		putBeeper();
 		moveToWall();
 		sweepOneRow();
 	}
 	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
 	private void faceEast() {
+		// HINT: is there anything different
+		// between this implementation faceEast
+		// and StoneMasonKarel's turnEast?
 		if (notFacingEast()) {
 			turnRight();
-			move();
 		}
 	}
-	private void putExtraBeeper() {
-		putBeeper();
-	}
 	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
 	private void moveToWall() {
 		while (frontIsClear()) {
 			move();
 		}
 		turnAround();
 	}
+	
+	/**
+	 * TODO: add description
+	 * @before:
+	 * @after:
+	 */
 	private void sweepOneRow() {
 		pickBeeper();
 		while (frontIsClear()) {
@@ -73,4 +120,11 @@ public class MidpointFindingKarel extends SuperKarel {
 			pickBeeper();
 		}
 	}
+	
+	// nala is the most beautiful nala and she is queen of the codes
+	// PS when bella was up with me last night I taught her all the
+	// answers so if you give her enough kisses and blueberries she
+	// might give you some hints.
+	//    also I gunna snuggle you color and give you all da kisses
+	//    when I get home to your colo
 }
